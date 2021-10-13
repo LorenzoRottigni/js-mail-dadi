f89dcdf366ca66f569694d6c74a5d2dfe71330e1
LorenzoRottigni <lorenzo@rottigni.net>
2021-10-13 17:53:13 +0200

    Dice Game Completo

---
+++ dice-game/dice-game.js
@@ -0,0 +1,29 @@
+const clientDice = document.querySelector('.col-10 div:nth-child(1) .dice');
+const serverDice = document.querySelector('.col-10 div:nth-child(2) .dice');
+const outputTarget = document.getElementById('output-target');
+const button = document.querySelector('button');
+
+button.addEventListener('click', function(){
+    clientDice.innerHTML = '';
+    serverDice.innerHTML = '';
+    const clientRand = Math.floor(Math.random() * 6);
+    const serverRand = Math.floor(Math.random() * 6);
+    for (let i = 0; i <= clientRand ; i++) {
+        diceDot = document.createElement("i");
+        clientDice.appendChild(diceDot);
+        diceDot.setAttribute('class', 'fas fa-dot-circle')
+    }
+    for (let i = 0; i <= serverRand ; i++) {
+        diceDot = document.createElement("i");
+        serverDice.appendChild(diceDot);
+        diceDot.setAttribute('class', 'fas fa-dot-circle')
+    }
+    if(clientRand > serverRand){
+        outputTarget.textContent = 'You Won'
+    }else if(clientRand === serverRand){
+        outputTarget.textContent = 'Tie'
+    }else{
+        outputTarget.textContent = 'You Lost'
+    }
+    
+})
