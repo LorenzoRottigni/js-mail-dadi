e371611ce3f0154c5fd2a897ba5b1dc53319b40c
LorenzoRottigni <lorenzo@rottigni.net>
2021-10-14 10:28:19 +0200

    Commenti e bug fix

--- dice-game/dice-game.js
+++ dice-game/dice-game.js
@@ -1,25 +1,36 @@
+//Dice of the user
 const clientDice = document.querySelector('.col-10 div:nth-child(1) .dice');
+//Dice of the server
 const serverDice = document.querySelector('.col-10 div:nth-child(2) .dice');
-const outputTarget = document.getElementById('output-target');
+//output elemenets
+const outputTarget = document.getElementById('output-target')
 const outputContainer = document.getElementById('output-container');
+
 const button = document.querySelector('button');
 
 button.addEventListener('click', function(){
+    //Default reset for appendChild
     clientDice.innerHTML = '';
     serverDice.innerHTML = '';
+    //Random number of the user
     const clientRand = Math.floor(Math.random() * 6);
+    //Random number of the server
     const serverRand = Math.floor(Math.random() * 6);
+    //Client dice's dots generator
     for (let i = 0; i <= clientRand ; i++) {
         diceDot = document.createElement("i");
         clientDice.appendChild(diceDot);
         diceDot.setAttribute('class', 'fas fa-dot-circle')
     }
+    //Server dice's dots generator
     for (let i = 0; i <= serverRand ; i++) {
         diceDot = document.createElement("i");
         serverDice.appendChild(diceDot);
         diceDot.setAttribute('class', 'fas fa-dot-circle')
     }
+    //from d-none to d-flex
     outputContainer.style.setProperty('display', 'flex', 'important');
+    //Output results
     if(clientRand > serverRand){
         outputTarget.textContent = 'You Won'
     }else if(clientRand === serverRand){
--- mail-checker/mail-checker.js
+++ mail-checker/mail-checker.js
@@ -1,16 +1,25 @@
+//Client mail
 const mailInput = document.getElementById('mail-input');
 const button = document.querySelector('button');
-const whiteList = ['lorenzo@rottigni.net','lorenzo@rottigni.tech','bolean@careers.com','hello@google.com'];
+//Mails allowed
+const whiteList = ['lorenzo@rottigni.net','lorenzo@rottigni.tech','boolean@careers.com','hello@google.com'];
+//Output elements
 const outputContainer = document.querySelector('.d-none');
 const outputTarget = document.getElementById('output-target');
 
 button.addEventListener("click", function(){
+    //Boolean var to stop the loop
     let accessGranted;
+    //drom d-none to d-block
+    outputContainer.style.setProperty('display', 'block', 'important');
+    //while i<array.lenght && access not granted
     for (let i = 0; i < whiteList.length && !accessGranted; i++) {
         const whiteListMail = whiteList[i];
+        //if inserted email = allowed email
         if(mailInput.value.toLowerCase() === whiteListMail.toLowerCase()){
-            outputContainer.style.setProperty('display', 'block', 'important');
+            //output
             outputTarget.textContent = 'access granted for '+ mailInput.value;
+            //Access granted ==> stop loop
             accessGranted = true
         }else
             outputTarget.textContent = 'access denied'
